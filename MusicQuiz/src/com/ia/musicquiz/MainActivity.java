a6e2683ee2f53e421c2878dd00303a5b0c89b3d9
danynab <danielmeanallorian@gmail.com>
2012-12-20 01:10:43 +0100

    Añadido código en el MainActivity.java para hacer una consulta a la base de datos, pero sin exito...

--- MusicQuiz/src/com/ia/musicquiz/MainActivity.java
+++ MusicQuiz/src/com/ia/musicquiz/MainActivity.java
@@ -1,15 +1,37 @@
 package com.ia.musicquiz;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
+
+import com.ia.musicquiz.persistence.SqliteManager;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);	
+		setContentView(R.layout.activity_main);
+
+		TextView tx = (TextView) findViewById(R.id.texto);
+		try {
+			SqliteManager.initBD(this);
+			SQLiteDatabase bd = SqliteManager.getDatabase();
+			Cursor c = bd.rawQuery("SELECT titulo,uri FROM cancion", null);
+
+			if (c.moveToFirst()) {
+				do {
+					String titulo = c.getString(0);
+					String uri = c.getString(1);
+					tx.setText(tx.getText().toString() + "\n" + titulo + "\n" + uri);
+				} while (c.moveToNext());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
